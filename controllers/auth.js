dac38e433f461154f491be32bbce1d9e6d7e9c62
hernan066 <hernan0066@gmail.com>
2021-11-03 03:31:44 -0300

    1.13 crear usuario db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,31 @@
 const express = require("express");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = (req, res = express.response) => {
+const crearUsuario = async (req, res = express.response) => {
   /* console.log(req.body); */
 
-  const { email, password, name } = req.body;
+  // const { email, password, name } = req.body;
 
-  
+  try {
+    const usuario = new Usuario(req.body);
 
-  return res.status(201).json({
-    ok: true,
-    msg: "register",
-    name,
-    email,
-    password,
-  });
+    await usuario.save();
+
+    return res.status(201).json({
+      ok: true,
+      msg: "register",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el admin",
+  })};
 };
 
 const login = (req, res = express.response) => {
   const { email, password } = req.body;
-  
-   
-  
+
   return res.status(201).json({
     ok: true,
     msg: "login",
