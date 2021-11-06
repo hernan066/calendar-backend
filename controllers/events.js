0f4699b83653c5054c263d560439b5e31bcefbf0
hernangit config --global user.name John <hernan0066@gmail.com>
2021-11-06 14:23:22 -0300

    2.8 Actualizar evento

--- controllers/events.js
+++ controllers/events.js
@@ -44,11 +44,50 @@ const crearEvento = async(req, res = response) => {
   
   
 }
-const actualizarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'actualizarEvento'
-      });
+const actualizarEvento = async(req, res = response) => {
+    
+      const eventoId = req.params.id;
+      const uid = req.uid;
+
+      try {
+
+        const evento = await Evento.findById(eventoId);
+
+        if(!evento){
+          return res.status(404).json({
+            ok: false,
+            msg: 'No existe el evento'
+          });
+        }
+         if (evento.user.toString() !== uid){
+            return res.status(401).json({
+              ok: false,
+              msg: 'No tienes permisos para actualizar este evento'
+            });
+        } 
+
+        const nuevoEvento = {
+          ...req.body,
+          user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, {new: true});
+
+        res.json({
+          ok: true,
+          evento: eventoActualizado
+        });
+        
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          ok: false,
+          msg: 'Hable con el admin'
+        });
+      }
+  
+  
+    
 }
 const eliminarEvento = (req, res = response) => {
     res.json({
